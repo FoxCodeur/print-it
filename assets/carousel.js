baae40b7196e4ab658c73bc4437a0b21bd367eda
Daisy <detcherry@gmail.com>
2024-12-04 15:28:13 +0100

    Add carousel functionality with dynamic dots creation. Import slides from external script.js. Implemented event listeners for prev and next buttons. Enabled module usage for better code structure.

--- assets/carousel.js
+++ assets/carousel.js
@@ -1,10 +1,23 @@
 // Sélection des éléments HTML
+import { slides } from "../assets/script.js";
 const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
-
+const dotsContainer = document.querySelector(".dots"); // Sélectionner directement <ul> pour ajouter les dots
 prevButton.addEventListener("click", () => {
   console.log("prev");
 });
 nextButton.addEventListener("click", () => {
   console.log("next");
 });
+
+const createDots = () => {
+  if (dotsContainer.children.length === 0) {
+    slides.forEach(() => {
+      const dot = document.createElement("li"); // Créer un nouvel élément <li>
+      dot.classList.add("dot"); // Ajouter la classe "dot"
+      dotsContainer.appendChild(dot); // Ajouter le dot au conteneur
+    });
+  }
+};
+// Appel de la fonction pour créer les dots
+createDots();
