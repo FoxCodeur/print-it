5f7d99d32eb59e6d2a1c38bed0565eba9e4e85c2
Daisy <detcherry@gmail.com>
2024-12-04 13:57:19 +0100

    Ajout des écouteurs d'événements sur les boutons 'prev' et 'next' pour tester les interactions du carrousel avec console.log

---
+++ assets/carousel.js
@@ -0,0 +1,10 @@
+// Sélection des éléments HTML
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
+prevButton.addEventListener("click", () => {
+  console.log("prev");
+});
+nextButton.addEventListener("click", () => {
+  console.log("next");
+});
