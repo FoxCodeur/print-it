859e456977cbd4b97d5f89a9ba8ad666112a0fde
Daisy <detcherry@gmail.com>
2024-12-09 09:56:16 +0100

    Ajout de la logique pour sélectionner un dot cliqué dans le carrousel : suppression de la classe 'dot_selected' pour tous les dots et ajout au dot actif.

--- assets/carousel.js
+++ assets/carousel.js
@@ -1,8 +1,12 @@
 // Sélection des éléments HTML
 import { slides } from "../assets/script.js";
+// Les variables
 const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
 const dotsContainer = document.querySelector(".dots"); // Sélectionner directement <ul> pour ajouter les dots
+// --------------------------------------------------------------------
+
+// Les fonctions
 prevButton.addEventListener("click", () => {
   console.log("prev");
 });
@@ -12,10 +16,20 @@ nextButton.addEventListener("click", () => {
 
 const createDots = () => {
   if (dotsContainer.children.length === 0) {
-    slides.forEach(() => {
+    slides.forEach((_, index) => {
       const dot = document.createElement("li"); // Créer un nouvel élément <li>
       dot.classList.add("dot"); // Ajouter la classe "dot"
       dotsContainer.appendChild(dot); // Ajouter le dot au conteneur
+      //--------------------------------------------------------------
+      dot.addEventListener("click", () => {
+        // console.log(`Dot cliqué : ${index}`); // Affiche l'index du dot cliqué dans la console
+        // Retirer "dot_selected" de tous les dots
+        const allDots = dotsContainer.querySelectorAll(".dot"); // Sélectionner tous les dots
+        allDots.forEach((d) => d.classList.remove("dot_selected")); // Supprimer la classe
+
+        // Ajouter la classe "dot_selected" uniquement au dot cliqué
+        dot.classList.add("dot_selected");
+      });
     });
   }
 };
